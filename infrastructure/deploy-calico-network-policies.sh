8022b0226d419ead633173accc03a3b959aa14d7
Martin Halamíček <martin@halamicek.cz>
2023-01-12 18:30:31 +0100

    install calico using operator

--- infrastructure/deploy-calico-network-policies.sh
+++ infrastructure/deploy-calico-network-policies.sh
@@ -2,8 +2,8 @@
 set -Eeuo pipefail
 
 # Installation - https://docs.aws.amazon.com/eks/latest/userguide/calico.html#calico-install
-kubectl apply -f ./calico-operator.yaml
-kubectl apply -f ./calico-crs.yaml
+kubectl create  -f ./calico-operator.yaml
+kubectl create  -f ./calico-crs.yaml
 
 
 kubectl apply -f <(cat <(kubectl get clusterrole aws-node -o yaml) ./append.yaml)
