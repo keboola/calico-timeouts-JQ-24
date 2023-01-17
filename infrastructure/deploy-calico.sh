576c53465d001225dcc5075e5b06b43109c19b82
Martin Halamíček <martin@halamicek.cz>
2023-01-17 13:12:03 +0100

    1. create new ip pool

--- infrastructure/deploy-calico.sh
+++ infrastructure/deploy-calico.sh
@@ -6,3 +6,5 @@ kubectl delete ds aws-node -n kube-system || true
 # Calico
 kubectl apply -f ./calico-cni.yaml
 kubectl rollout status daemonset/calico-node --timeout=600s -n kube-system
+
+kubectl apply -f ./calico-resources.yaml
